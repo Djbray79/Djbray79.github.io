4deeeefea94d3d9836220023c7ce4259710b2527
Davis Brayall <djbray79@yahoo.com>
2020-06-08 18:39:34 -0400

    combined css to 1 file and added first javascript function

---
+++ js/javas.js
@@ -0,0 +1,7 @@
+var person = prompt("Welcome to my portfolio, please tell me your name?");
+
+if (person == null || person == "") {
+  txt = "User cancelled the prompt.";
+} else {
+  alert("Thank you for visiting my portfolio " + person + ".");
+} 
